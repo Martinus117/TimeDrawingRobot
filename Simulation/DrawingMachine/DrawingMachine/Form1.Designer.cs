e4f12867ff941d1d1cd45d11d630064418c7ea05
Martin Keller Franzi <martinus117@bluewin.ch>
2017-09-26 21:59:57 +0200

    Additional files for initial git.

---
+++ Simulation/DrawingMachine/DrawingMachine/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace DrawingMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(12, 526);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Background";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(766, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(278, 503);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(93, 527);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 21);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "EdgePoints";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button3.Location = new System.Drawing.Point(174, 525);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Outline";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button4.Location = new System.Drawing.Point(766, 521);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox1.Location = new System.Drawing.Point(367, 528);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "2";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button5.Location = new System.Drawing.Point(286, 528);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Letter";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button6.Location = new System.Drawing.Point(473, 526);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Letter Outline";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(665, 531);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Count:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(709, 531);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "label2";
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button7.Location = new System.Drawing.Point(573, 525);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "exp";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 498);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "ShowLines";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 561);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Simulation/DrawingMachine/DrawingMachine/Form1.cs
@@ -0,0 +1,432 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawingMachine
+{
+
+    public partial class Form1 : Form
+    {
+        public string badPoints;
+        public Graphics g;
+        const Double lInner = 450;
+        const Double lOuter = 540;
+        const Double pWidth = 900;
+        const Double pHeight = 530;
+        
+        const int rX = 150;
+        const int rY = 10;
+        const float cFactor = 5.6F;
+        PointF zPoint = new PointF(450, -250);  // from mPoint
+        PointF mPoint = new PointF(150, 540); // from WindowsCoordinates
+        Double iMax = 0, iMin=360, oMax=0, oMin=360;
+        // int maxP = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Width = 1500;
+            this.Height = 1000;
+            g = this.CreateGraphics();
+            g.Clear(Color.White);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            DrawBackground();       
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DrawPoint(0, 0);
+            DrawPoint(0, pHeight);
+            DrawPoint(pWidth, pHeight);
+            DrawPoint(pWidth, 0);
+        }
+
+        private void DrawPoint(double xx, double yy)
+        {
+            PointF t1 = new PointF((float)xx, (float)yy);
+            double oAngleRad = 0, iAngleRad = 0;
+            InnerOuterAngles(t1, ref iAngleRad, ref oAngleRad);
+
+            double oAngle = Rad2Deg(oAngleRad);
+
+            double iAngle = Rad2Deg(iAngleRad);
+            textBox2.Text += "x=" + (t1.X.ToString("000") + "  y=" + t1.Y.ToString("000") + " I:" + iAngle.ToString("###0.00") + " O:" + oAngle.ToString("###0.00") + Environment.NewLine);
+            DrawParts(t1, Color.Purple);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int iSleep = 150; // 50
+            int iStep = 25;  // 10
+            for (int ii = 0; ii <= pHeight; ii += iStep)
+            {
+                System.Threading.Thread.Sleep(iSleep);
+                this.Show();
+                Color c = new Color();
+                c = Color.FromArgb(10, 10, 10);
+                DrawParts(new PointF(0,ii), c);
+            }
+          
+            for (int ii = 0; ii <= pWidth; ii += iStep)
+            {
+                System.Threading.Thread.Sleep(iSleep);
+                this.Show();
+                DrawParts(new PointF(ii, 530), Color.Blue);
+            }
+
+            for (int ii = (int)pHeight; ii >= 0; ii -= iStep)
+            {
+                System.Threading.Thread.Sleep(iSleep);
+                this.Show();
+                DrawParts(new PointF((float)pWidth, ii), Color.Green);
+            }
+
+            for (int ii = (int)pWidth; ii >= 0; ii -= iStep)
+            {
+                System.Threading.Thread.Sleep(iSleep);
+                this.Show();
+                DrawParts(new PointF(ii, 0), Color.Gold);
+            }
+            textBox2.Text += "I_Max:" + iMax.ToString("###0.00") + "I_Min:" + iMin.ToString("###0.00") + Environment.NewLine;
+            textBox2.Text += "O_Max:" + oMax.ToString("###0.00") + "O_Min:" + oMin.ToString("###0.00") + Environment.NewLine;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            label2.Text = "";
+            DrawBackground();
+            Double maxL = Math.Sqrt(Math.Pow(zPoint.X, 2) + Math.Pow((pHeight + Math.Abs(zPoint.Y)), 2));
+            textBox2.Text = "Max L: " + maxL.ToString();
+        }
+
+
+        private void DrawBackground()
+        {
+            Pen p = new Pen(Color.Blue, 1);
+            Rectangle rect = new Rectangle(rX, rY, (int)pWidth, (int)pHeight);
+            p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            g.DrawRectangle(p, rect);
+            DrawPoint(zPoint);
+            DrawPoint(0, 0);
+            DrawPoint(900, 530);
+            DrawPoint(900, 0);
+            DrawPoint(0, 530);
+            DrawPoint(100, 100);
+            DrawPoint(10, 80);
+            DrawPoint(12, 80);
+        }
+
+        private void DrawParts(PointF pf, Color color)
+        {
+            double oAngleRad=0, iAngleRad=0;
+            InnerOuterAngles(pf, ref iAngleRad, ref oAngleRad);           
+            
+            double lx = lInner * Math.Sin(iAngleRad);
+            double ly = lInner * Math.Cos(iAngleRad);
+            double txi = zPoint.X - lx;
+            double tyi = zPoint.Y - ly;
+
+            if (checkBox1.Checked)
+            {
+                g.DrawLine(new Pen(System.Drawing.Color.Yellow, 2), M2Win(zPoint), M2Win(new PointF((float)txi, (float)tyi)));
+                Point iPoint = M2Win(new PointF((float)txi, (float)tyi));
+                g.DrawEllipse(new Pen(color, 2), iPoint.X - 2, iPoint.Y - 2, 4, 4);
+            }
+
+            double ax = oAngleRad - iAngleRad;
+            lx = lOuter * Math.Sin(ax);
+            ly = lOuter * Math.Cos(ax);
+            double txo = txi - lx;
+            double tyo = tyi + ly;
+
+            double iAngle = Rad2Deg(iAngleRad);
+            double oAngle = Rad2Deg(oAngleRad);
+            if (iAngle > iMax)
+                iMax = iAngle;
+            if (iAngle < iMin)
+                iMin = iAngle;
+            if (oAngle > oMax)
+                oMax = oAngle;
+            if (oAngle < oMin)
+                oMin = oAngle;
+
+            Point oPoint = M2Win(new PointF((float)txo, (float)tyo));
+            g.DrawEllipse(new Pen(color, 1), oPoint.X - 1, oPoint.Y - 1, 2, 2);
+
+            if (checkBox1.Checked)
+            {
+                g.DrawLine(new Pen(color, 2), M2Win(new PointF((float)txi, (float)tyi)), M2Win(new PointF((float)txo, (float)tyo)));
+            }
+       }
+
+        private void InnerOuterAngles(PointF pf, ref Double iAngle, ref Double oAngle)
+        {
+            Double xx, yy, zz;
+            // li und lo unterschiedlich
+            // Hypothenuse z berechnen aus x, y (z = SQRT(x2 + y2)
+            // x: Fallunterscheidung
+            // Cosinus-Satz: c2 = a2 + b2 -2abcos(y)
+            if (pf.X <= zPoint.X)
+                xx = zPoint.X - pf.X;
+            else
+                xx = pf.X - zPoint.X;
+            yy = pf.Y + Math.Abs(zPoint.Y);
+            zz = Math.Sqrt(xx * xx + yy * yy);
+
+            // Outer Angle
+            oAngle = Math.Acos((lInner * lInner + lOuter * lOuter - zz * zz) / (2 * lInner * lOuter));
+            
+            // inner Angle
+            Double alpha1 = Math.Acos((zz * zz + lInner * lInner - lOuter * lOuter) / (2 * lInner * zz));
+            Double alpha2 = Math.Asin(xx / zz);
+            if (pf.X < zPoint.X)
+                iAngle = Math.PI - alpha1 - alpha2;
+            else
+                iAngle = Math.PI + alpha2 - alpha1;
+        }
+
+        private double Rad2Deg(double rad)
+        {            
+            return rad * (180.0 / Math.PI);            
+        }
+
+        private void DrawPoint(PointF p)
+        {
+            Point pt = M2Win(p);
+            Pen pen = new Pen(System.Drawing.Color.Red, 1);
+            g.DrawEllipse(pen, pt.X - 2, pt.Y - 2, 4, 4);
+        }
+
+        private void DrawPoint(int x, int y)
+        {
+            DrawPoint(new PointF(x, y));
+        }
+
+        private Point M2Win(PointF p)
+        {
+            return new Point((int)(p.X + mPoint.X), (int)(mPoint.Y - p.Y));
+        }
+
+        private PointF Win2M(PointF p)
+        {
+            return new PointF(mPoint.X - p.X, p.Y - mPoint.Y);
+        }
+
+
+        private GraphicsPath PrepLetter(string letter)
+        {
+            // FontFamily ff = new FontFamily("1CamBam_Stick_2");
+            // FontFamily ff = new FontFamily("Kunstler Script");
+            // FontFamily ff = new FontFamily("Arial");
+            FontFamily ff = new FontFamily("Kristen ITC");
+            GraphicsPath gp = new System.Drawing.Drawing2D.GraphicsPath();
+            gp.AddString(letter, ff, (int)FontStyle.Regular, 350, new Point(0, 0), StringFormat.GenericTypographic);
+            return gp;
+        }
+
+        private void DrawPath(GraphicsPath gp)
+        {
+            int effPoints = 0;
+            PointF p1, p2;
+            float factor = 1.0F;
+            Pen pen = new Pen(Color.Red, 2);
+            Color lastColor = pen.Color;
+            // dispPoint(0, gp.PathPoints[0], gp.PathTypes[0], pen.Color);
+            for (int ii = 0; ii < gp.PointCount; ii++)
+            {
+                int ll = 1;                
+                String iiString = ";" + ii.ToString();
+                if (badPoints.IndexOf(iiString) > 0 && ii < gp.PointCount - 1)
+                {
+                    ii++;
+                    ll++;
+                }
+
+                if (ii > 0)
+                {
+                    effPoints++;
+                    p1 = gp.PathPoints[ii - ll];
+                    p2 = gp.PathPoints[ii];
+                    p1.Y = p1.Y * factor;
+                    p1.Y = mPoint.Y - p1.Y;
+                    p2.Y = p2.Y * factor;
+                    p2.Y = mPoint.Y - p2.Y;
+                    p1.X = p1.X * factor;
+                    p2.X = p2.X * factor;
+
+                    if (gp.PathTypes[ii] == 0)
+                    {
+                        lastColor = pen.Color;
+                        pen.Color = Color.Yellow;
+                    }
+                    else if (gp.PathTypes[ii - ll] == 0)
+                    {
+                        if (lastColor == Color.Red) pen.Color = Color.Blue;
+                        else pen.Color = Color.Red;
+                    }
+                    DrawParts(p1, pen.Color);
+                    System.Threading.Thread.Sleep(10);
+
+                    if (ii + 1 == gp.PointCount)
+                        DrawParts(p2, pen.Color);
+                }
+            }
+            label2.Text = gp.PointCount.ToString() + " : " + effPoints.ToString();
+        }
+
+        private void DrawPathLine(GraphicsPath gp)
+        {
+            PointF p1, p2;
+            float factor = 3.5F;
+            factor = 1.0F;
+            Pen pen = new Pen(Color.Red, 2);
+            Color lastColor = pen.Color;
+            // ;xx;yy
+            int effPoints = 0; 
+            // dispPoint(0, gp.PathPoints[0], gp.PathTypes[0], pen.Color);
+            for (int ii = 0; ii < gp.PointCount; ii++)
+            {
+                int ll = 1;
+                String iiString = ";" + ii.ToString();
+                if (badPoints.IndexOf(iiString) > 0) { 
+                    ii++;
+                    ll++;
+                }
+
+                if (ii > 0)
+                {
+                    effPoints++;
+                    p1 = gp.PathPoints[ii - ll];
+                    p2 = gp.PathPoints[ii];
+                    p1.Y = p1.Y * factor;
+                    p1.Y = mPoint.Y - p1.Y;
+                    p2.Y = p2.Y * factor;
+                    p2.Y = mPoint.Y - p2.Y;
+                    p1.X = p1.X * factor;
+                    p2.X = p2.X * factor;
+                    if (gp.PathTypes[ii] == 0)
+                    {
+                        lastColor = pen.Color;
+                        pen.Color = Color.Yellow;
+                    }
+                    else if (gp.PathTypes[ii - ll] == 0)
+                    {
+                        if (lastColor == Color.Red) pen.Color = Color.Blue;
+                        else pen.Color = Color.Red;
+                    }
+                    g.DrawLine(pen, M2Win(p1), M2Win(p2));
+                    System.Threading.Thread.Sleep(20);
+                }
+            }
+            label2.Text = gp.PointCount.ToString() + " : " + effPoints.ToString();
+            textBox2.Refresh();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        { 
+            GraphicsPath gp = PrepLetter(textBox1.Text);
+            testSmall(gp);
+            // (gp.Flatten();        
+            DrawPath(gp);
+            //DrawPathLine(gp);
+            SolidBrush brush = new SolidBrush(Color.Black);
+
+        }
+
+        private void writeGp(GraphicsPath g)
+        {
+            textBox2.Text = "double Matrix[][2] = { \r\n";
+            for (int ii=0;ii<g.PointCount; ii++)
+            {
+                String iiString = ";" + ii.ToString();
+                if (!(badPoints.IndexOf(iiString) > 0))
+                {
+                    textBox2.Text += ii.ToString() 
+                        +  "= {" + g.PathPoints[ii].X.ToString("0.00") 
+                        + ", " + g.PathPoints[ii].Y.ToString("0.00") + "},  " 
+                        + g.PathTypes[ii].ToString() +  "\r\n";
+                }
+            }
+            textBox2.Text += "};";
+        }
+
+        private void writeGpSmall(GraphicsPath g)
+        {
+            float x, y;
+            textBox2.Text = "";
+            float maxY = 0;
+            for (int ii = 0; ii < g.PointCount; ii++)
+            {
+                if (g.PathPoints[ii].Y > maxY)
+                    maxY = g.PathPoints[ii].Y;
+            }
+            textBox2.Text = "maxY: " + maxY.ToString() + Environment.NewLine;
+            
+            for (int ii = 0; ii < g.PointCount; ii++)
+            {
+                 String iiString = ";" + ii.ToString();
+                if (!(badPoints.IndexOf(iiString) > 0))
+                {
+                    x = (g.PathPoints[ii].X) / cFactor;
+                    y = (450 - g.PathPoints[ii].Y) / cFactor;
+                    textBox2.Text += x.ToString("0.00") + "," 
+                        + y.ToString("0.00") + "," +                        
+                        (g.PathTypes[ii] != 1 ? g.PathTypes[ii].ToString() + ";" : ";");
+                }
+            }
+        }
+
+        private void testSmall(GraphicsPath _gp)
+        {
+            badPoints = "";
+            _gp.Flatten();
+            int count = _gp.PointCount;
+            PointF pf;
+            pf = _gp.PathPoints[0];
+
+            for (int i = 1; i < count; i++)
+            {
+                if ((Math.Abs(_gp.PathPoints[i].X - pf.X) < 2
+                     && Math.Abs(_gp.PathPoints[i].Y - pf.Y) < 2)    // 1
+                     && _gp.PathTypes[i] == 1)
+                {
+                    badPoints += ";" + i.ToString();
+                    i++;
+                }
+                else
+                    pf = _gp.PathPoints[i];
+            }
+        }
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            GraphicsPath gp = PrepLetter(textBox1.Text);
+            testSmall(gp);
+            writeGp(gp);
+            DrawPathLine(gp);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            GraphicsPath gp = PrepLetter(textBox1.Text);
+            testSmall(gp);
+            writeGpSmall(gp);
+        }
+    }
+}
